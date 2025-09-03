e35f778dda38a7b9beeac29175c4490f88ebc0dd
utkarsh <utkarsh@utkarshs-MacBook-Air.local>
2025-09-03 15:04:50 +0530

    origin change

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 const app=express()
 app.use(
     cors({
-      origin: 'http://localhost:5173',
+      origin: 'https://movie-pie-frontend.vercel.app/',
       credentials: true,
     })
   );
