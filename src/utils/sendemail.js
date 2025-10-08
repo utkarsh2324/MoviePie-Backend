39ced1fa30e089c480dcb6fca89683f7654d70c9
utkarsh <utkarsh@utkarshs-MacBook-Air.local>
2025-10-08 22:38:41 +0530

    port change for gmail

--- src/utils/sendemail.js
+++ src/utils/sendemail.js
@@ -4,7 +4,9 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to, subject, message) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,        // TLS port
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
